193aea27f1adfebc6b6f92a0ba08b8e437598086
achiever-zbw <zhongkao0621@outlook.com>
2024-12-03 20:39:25 +0800

    数据增强，图片换成128*128的输入，卷积核扩大，防止过拟合

--- CNN_model.py
+++ CNN_model.py
@@ -10,137 +10,97 @@ import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-from torchvision.models import resnet18
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from Denoising import medianblur #调用去噪库中的中值滤波函数
+from Denoising import medianblur  # 调用去噪库中的中值滤波函数
 
-#待去噪的图片路径
-non_denoising_crack_path='./crack-identify/train_images/crack'
-non_denoising_noncrack_path='./crack-identify/train_images/non_crack'
+# 待去噪的图片路径
+non_denoising_crack_path = './crack-identify/train_images/crack'
+non_denoising_noncrack_path = './crack-identify/train_images/non_crack'
 
-#去噪后的图片路径
-denoising_crack_path='./crack-identify/Denoising_train_images/crack'
-denoising_noncrack_path='./crack-identify/Denoising_train_images/non_crack'
+# 去噪后的图片路径
+denoising_crack_path = './crack-identify/Denoising_train_images/crack'
+denoising_noncrack_path = './crack-identify/Denoising_train_images/non_crack'
 
-#中值滤波器窗口大小，必须是奇数
-kernal_size=5
-medianblur(non_denoising_noncrack_path,denoising_noncrack_path,kernal_size)
-medianblur(non_denoising_crack_path,denoising_crack_path,kernal_size)
-"""
-crack_path='./dataset/crack.txt'
-with open(crack_path,'r') as f:
-    crack_image=[line.strip() for line in f.readlines()]
-    
-non_crack_path='./dataset/non_crack.txt'
-with open(non_crack_path,'r') as f:
-    non_crack_image=[line.strip() for line in f.readlines()]
-    
-train_image_path='./dataset/images/train'
-train_crack_image_path='./dataset/train_crack_image'
-train_noncrack_image_path='./dataset/train_noncrack_image'
+# 中值滤波器窗口大小，必须是奇数
+kernal_size = 5
+medianblur(non_denoising_noncrack_path, denoising_noncrack_path, kernal_size)
+medianblur(non_denoising_crack_path, denoising_crack_path, kernal_size)
 
-for crack_image_name in crack_image:
-    source_path = os.path.join(train_image_path,crack_image_name )  # 构造源路径
-    destination_path = os.path.join(train_crack_image_path, crack_image_name)  # 构造目标路径
-    
-    if os.path.exists(source_path):
-       shutil.copy(source_path, destination_path)  # 复制文件到目标文件夹
-       print(f"Copied { crack_image_name} to {train_crack_image_path}")
-    else:
-       print(f"Image {crack_image_name} not found in {train_image_path}")
-       
-for non_crack_image_name in non_crack_image:
-    source_path = os.path.join(train_image_path,non_crack_image_name )  # 构造源路径
-    destination_path = os.path.join(train_noncrack_image_path, non_crack_image_name)  # 构造目标路径
-    
-    if os.path.exists(source_path):
-       shutil.copy(source_path, destination_path)  # 复制文件到目标文件夹
-       print(f"Copied { non_crack_image_name} to {train_noncrack_image_path}")
-    else:
-       print(f"Image {non_crack_image_name} not found in {train_image_path}")
+data_path = './crack-identify/Denoising_train_images'
 
-"""
-
-data_path='./crack-identify/Denoising_train_images'
-
-#数据预处理 
-transform=transforms.Compose([
-    transforms.Resize([256,256]),
+# 数据预处理
+transform = transforms.Compose([
+    transforms.Resize([128, 128]),  # 输入调整为128x128
+    transforms.RandomHorizontalFlip(),  # 随机水平翻转
+    transforms.RandomVerticalFlip(),  # 随机垂直翻转
     transforms.ToTensor(),
-    transforms.Normalize([0.5],[0.5])
-    ])
+    transforms.Normalize([0.5], [0.5])
+])
 
-#加载数据集
-dataset=datasets.ImageFolder(root=data_path,transform=transform)
-dataloader=DataLoader(dataset,32,shuffle=True)
+# 加载数据集
+dataset = datasets.ImageFolder(root=data_path, transform=transform)
+dataloader = DataLoader(dataset, 32, shuffle=True)
 
-#获取类别
-classes=dataset.classes
-sum_classes=len(classes)
-#print(f"类别：{classes}")
+# 获取类别
+classes = dataset.classes
+sum_classes = len(classes)
 
-#构建CNN模型
+# 构建CNN模型
 
 class CNN(nn.Module):
     def __init__(self, num_classes):
         super(CNN, self).__init__()
         self.conv_layers = nn.Sequential(
-            #卷积层
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),  # 输入通道=3，输出通道=32 ,卷积核大小3*3，步长为1，填充为1
+            nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=2),  # 输入通道=3，输出通道=32 ,卷积核大小5*5，步长为1，填充为2
             nn.ReLU(),   # 激活函数
-            
-            #池化操作(最大池化)
-            nn.MaxPool2d(kernel_size=2, stride=2),                 # 尺寸减半：256*256 到 128*128（通过2*2的池化窗口实现）
-            nn.Conv2d(32,32, kernel_size=3, stride=1, padding=1), # 输入通道=32保持不变
-            #nn.Conv2d(32,64, kernel_size=3, stride=1, padding=1)  #32到64
-            nn.ReLU(),                                             # 激活函数
-            nn.MaxPool2d(kernel_size=2, stride=2),                 # 尺寸减半：128*128 到 64*64
+
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 尺寸减半：128*128 到 64*64
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 32到64
+            nn.ReLU(),  # 激活函数
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 尺寸减半：64*64 到 32*32
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # 64到128
+            nn.ReLU(),  # 激活函数
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 尺寸减半：32*32 到 16*16
         )
-        #全连接层
+
+        # 全连接层
         self.fc_layers = nn.Sequential(
-            nn.Flatten(),     # 展平特征图
-            nn.Linear(32 * 64 * 64, 128),     # 输入维度=64*64*64，输出=128
-            nn.ReLU(),                                             # 激活函数
-            nn.Linear(128, num_classes),                          # 输出层，分类数=num_classes
+            nn.Flatten(),  # 展平特征图
+            nn.Linear(128 * 16 * 16, 512),  # 输入维度=128*16*16，输出=512
+            nn.ReLU(),  # 激活函数
+            nn.Linear(512, num_classes),  # 输出层，分类数=num_classes
         )
 
     def forward(self, x):
         x = self.conv_layers(x)  # 通过卷积层
-        x = self.fc_layers(x)    # 通过全连接层
+        x = self.fc_layers(x)  # 通过全连接层
         return x
-    
-num_classes = len(classes)
-model = CNN(num_classes=num_classes)  
 
+
+num_classes = len(classes)
+model = CNN(num_classes=num_classes)
 
 # 定义损失函数和优化器
 loss = nn.CrossEntropyLoss()  # 损失函数
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0005)
 
-
-#训练模型
-num_epochs =15
+# 训练模型
+num_epochs = 15
 for each in range(num_epochs):
     model.train()  # 设置模型为训练模式
     running_loss = 0.0
     correct = 0.0
     total = 0.0
-    
-    #model.train()  # 设置模型为训练模式
 
     for i, (inputs, labels) in enumerate(dataloader, 0):
-        # 将数据送到设备
-        
-        
         optimizer.zero_grad()  # 梯度清零, 每次训练前防止梯度叠加
         outputs = model(inputs)  # 向前传播
         loss_data = loss(outputs, labels)  # 计算损失
-        
+
         loss_data.backward()  # 反向传播
         optimizer.step()  # 优化器更新参数
-        
+
         # 累加损失
         running_loss += loss_data.item()
 
@@ -155,18 +115,5 @@ for each in range(num_epochs):
     print(f'Epoch [{each+1}/{num_epochs}], Loss: {each_loss:.2f}, Accuracy: {each_acc:.2f}%')
 
 # 保存模型
-torch.save(model, 'cnn_model.pkl') #修改pth格式为pkl
+torch.save(model, 'cnn_model.pkl')  # 修改pth格式为pkl
 print("模型已保存")
-
-
-
-
-
-
-
-
-
-
-
-
-
