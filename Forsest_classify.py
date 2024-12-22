6b12e8c75faa1ee70a6e763efe1eca961d910cd0
achiever-zbw <zhongkao0621@outlook.com>
2024-12-22 12:49:17 +0800

    完成随机森林训练与预测

--- Forsest_classify.py
+++ Forsest_classify.py
@@ -5,7 +5,11 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from CNN_model import CNN_feature_take
+from Verify import right_labels
+import cv2
+import os
+from PIL import Image
+from Denoising import medianblur
 
 transform = transforms.Compose([
     transforms.Resize([128, 128]),
@@ -18,14 +22,12 @@ data_path = './crack-identify/Denoising_train_images'
 dataset = datasets.ImageFolder(root=data_path, transform=transform)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-model = CNN_feature_take(num_classes=2)
-model.eval()
 
 # 提取特征，准备用于随机森林训练
 
 
 # 提取特征并展平图像
-def features_take(dataloader):
+def features_take_train(dataloader):
     features = []
     labels = []
     for inputs, label in dataloader:
@@ -34,21 +36,74 @@ def features_take(dataloader):
         features.append(inputs.numpy())  # 将特征转换为NumPy数组
         labels.append(label.numpy())  # 将标签转换为NumPy数组
 
-    features = np.vstack(features)  # 将所有的特征堆叠在一起，形成一个大数组
+    features = np.vstack(features)   # 将所有的特征堆叠在一起，形成一个大数组
     labels = np.hstack(labels)  # 将所有的标签堆叠在一起
     return features, labels
 
 
-features, labels = features_take(dataloader)
+features_train, labels_train = features_take_train(dataloader)
 # print(labels)
 # print(features)
-# print(len(features))
-# print(len(labels))
-# # 使用随机森林进行训练
-# X_train, X_test, y_train, y_test = train_test_split(
-#     features, labels, test_size=0.2, random_state=42)
-# rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-# rf_classifier.fit(X_train, y_train)
-# y_pred = rf_classifier.predict(X_test)
-# accuracy = accuracy_score(y_test, y_pred)
-# print(f'随机森林训练准确率: {accuracy * 100:.2f}%')
+print(len(features_train))
+print(len(labels_train))
+
+# 使用随机森林进行训练
+rf_classify = RandomForestClassifier(n_estimators=100, random_state=42)
+# 使用整个训练集进行训练
+rf_classify.fit(features_train, labels_train)
+# fit(x,y)  x-->特征矩阵，二维的结构  y-->标签，一维数组
+print("随机森林训练完毕")
+
+
+# ----------------------进行验证--------------------------
+
+
+# 定义路径
+Right_labels_path = './crack-identify/Right_labels.txt'
+right_label_dict = right_labels(Right_labels_path)
+
+# 定义路径
+verify_images_path = './crack-identify/verify_images'
+denoising_verify_images_path = './crack-identify/Denoising_verify_images'
+final_path = './crack-identify/final'
+kernel_size = 5  # 中值滤波器窗口大小
+
+# 去噪验证集图片
+medianblur(verify_images_path, denoising_verify_images_path, kernel_size)
+
+verify_images = [os.path.join(denoising_verify_images_path, f)
+                 for f in os.listdir(denoising_verify_images_path)]
+
+
+def features_take_verify(verify_images, transform):
+    features = []
+    for img_path in verify_images:
+        img = Image.open(img_path)
+        img_tensor = transform(img).unsqueeze(0)
+        img_tensor = img_tensor.view(img_tensor.size(0), -1)  # 展平为一维向量
+        features.append(img_tensor.numpy())
+    features = np.vstack(features)
+    return features
+
+
+features_verify = features_take_verify(verify_images, transform)
+print(f"验证集图片数量为{len(features_verify)}")
+
+# 获取验证集标签
+verify_labels = []
+for img_path in verify_images:
+    img_name = os.path.basename(img_path)
+    label = right_label_dict[img_name]  # 获取标签
+    if label == "crack":
+        label_new = 0
+        verify_labels.append(label_new)
+    else:
+        label_new = 1
+        verify_labels.append(label_new)
+print(verify_labels)
+print(len(verify_labels))
+# 对验证集进行预测
+verify_predictions = rf_classify.predict(features_verify)
+# 计算准确率
+accuracy = accuracy_score(verify_labels, verify_predictions)
+print(f"验证集准确率: {accuracy * 100:.2f}%")
