3e0f35b302e6be25992b4d0ffbf639e6d8c360e6
achiever-zbw <zhongkao0621@outlook.com>
2024-12-25 20:37:41 +0800

    中值滤波去噪函数

--- Denoising.py
+++ Denoising.py
@@ -1,20 +1,9 @@
 import os
 import cv2
 
-def medianblur(input_path,output_path,kernel_size):
-    """
-    #待去噪的图片路径
-    non_denoising_crack_path='./crack-identify/train_images/crack'
-    non_denoising_noncrack_path='./crack-identify/train_images/non_crack'
 
-    #去噪后的图片路径
-    denoising_crack_path='./crack-identify/Denoising_train_images/crack'
-    denoising_noncrack_path='./crack-identify/Denoising_train_images/non_crack'
-
-    #中值滤波器窗口大小，必须是奇数
-    kernal_size=5
-    """
-    #对指定位置的图片进行中值滤波去噪
+def medianblur(input_path, output_path, kernel_size):
+    # 对指定位置的图片进行中值滤波去噪
     for name in os.listdir(input_path):
         input_img_path = os.path.join(input_path, name)
         output_img_path = os.path.join(output_path, name)
@@ -27,7 +16,8 @@ def medianblur(input_path,output_path,kernel_size):
 
         # 保存去噪后的图片
         cv2.imwrite(output_img_path, denoised_img)
-        
+
+
 # 待去噪的图片路径
 non_denoising_crack_path = './crack-identify/train_images/crack'
 non_denoising_noncrack_path = './crack-identify/train_images/non_crack'
